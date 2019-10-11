23435f13e4fc446a6e62462abc440984061c1949
achilep <pahonsibebeto@gmail.com>
2019-10-11 09:06:39 -0700

    fix bug on application properties

--- src/main/java/com/catalog/com/ApiCatalogApplication.java
+++ src/main/java/com/catalog/com/ApiCatalogApplication.java
@@ -2,11 +2,7 @@ package com.catalog.com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 public class ApiCatalogApplication {
