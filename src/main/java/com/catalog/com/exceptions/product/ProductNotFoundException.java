aed1da2a5775976d47cd228ca2624aa55543932c
ndzib <brunoserkwi@gmail.com>
2019-10-18 22:16:56 +0000

    addeded actuator for application monitoring

--- src/main/java/com/catalog/com/exceptions/product/ProductNotFoundException.java
+++ src/main/java/com/catalog/com/exceptions/product/ProductNotFoundException.java
@@ -3,8 +3,6 @@ package com.catalog.com.exceptions.product;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class ProductNotFoundException extends RuntimeException {
